3f1bc263db7340863a344578fddf4c01217c434d
Anna <kira8721@mail.ru>
2022-12-15 23:12:16 +0300

    Добавила задачи

---
+++ HelloPython/Hello.py
@@ -0,0 +1,50 @@
+# value = None
+#print(type(value))
+
+# a = 123
+# b = 1.23
+#print(type(a))
+#print(type(b))
+#value = 12334
+# s = 'hello world'
+
+# print((s)) #вывод строки
+# print(s,'-',b,'-',a)
+# f=True
+# print(f)
+
+# username = input('Введите имя:')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина')
+# elif username == 'Даниил':
+#     print('Даниил-топ)')
+# else:
+#     print('Привет, ', username)
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# print(inverted)
+
+# list = [1,2,3,10,5]
+# for i in list:
+#     print(i)
+
+# r = range(10)
+# for i in r:
+#     print(i)
+# или
+# for i in range(5):
+#     print(i)
+
+
+a = 5
+a*=a
+print(a)
+
+
+
