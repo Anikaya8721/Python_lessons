0909a741d70067bebc5c260251567c4db97e5d55
Anna <kira8721@mail.ru>
2023-01-04 15:24:30 +0300

    hw3

--- Hw_2/hw2_3.py
+++ Hw_2/hw2_3.py
@@ -3,3 +3,6 @@
 #  in 6
 # out [2.0, 2.25, 2.37, 2.441, 2.488, 2.552]
 # 14.071
+n = int(input())
+list = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+print(f'Последовательность: {list}\n Сумма: {round(sum(list), 3)}')
