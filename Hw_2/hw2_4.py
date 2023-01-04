1e37a24299f79c7af28043088a963a27ab6b24f0
Anna <kira8721@mail.ru>
2023-01-04 15:40:28 +0300

    hw4

--- Hw_2/hw2_4.py
+++ Hw_2/hw2_4.py
@@ -1,3 +1,16 @@
 # Написать прогу, которая принимает на вход 2 числа.
 # Получите значение N для пустого списка заполните числами в диапазоне [-N, N].
 # Найдите произведение элементов на указанных позициях (не индексах).
+# number1 = int(input("Input number1: "))
+# number2 = int(input("Input number2: "))
+# Чтение чисел из файла
+# number1 = str(input("Input number1: "))
+# number2 = str(input("Input number2: "))  
+# number1, number2 = int(number1)-1, int(number2)-1# Перевод строк в числа, в указатели на индексы
+# N = int(input("Введите количество чисел: "))
+# A = []  # Формирование массива чисел от -N до N
+# for i in range(-N, N+1):
+#     A.append(i)
+# res = A[number1] * A[number2]  # Перемножение указанных позиций
+# print(f"Position one: {number1+1}\n Position two: {number2+1}\nNumber of elements: {N}\n{A}\n{res}")  # Вывод результатов
+
