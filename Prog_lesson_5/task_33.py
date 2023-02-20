42273a75618240384db9d1e209da224af16dd86e
Anna <kira8721@mail.ru>
2023-02-20 18:54:45 +0300

    enter tasks and hw

---
+++ Prog_lesson_5/task_33.py
@@ -0,0 +1,18 @@
+# Хакер получил к классному журналу и хочет заменить все свои
+# минимальные оценки на максимальные.
+# Напишите программу, которая заменяет оценки Василия, но наоборот:
+# все максимальные, на минимальные.
+# Input: 5 -> 1 3 3 3 4
+# Ouyput: 1 3 3 3 1
+# import random
+
+# list1 = [random.randint(1, 5) for i in range (int(input()))]
+# print(list1)
+
+# maxM = max(list1)
+# minM = min(list1)
+
+# for i in range(len(list1)):
+#     if list1[i] == maxM:
+#         list1[i] = minM
+# print(list1)
